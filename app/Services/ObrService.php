5c44facf913deb715d89d409bd170d86c72c9e0f
Jean-Lionel <nijeanlionel@gmailcom>
2026-01-13 06:46:58 +0200

    define post methods

--- app/Services/ObrService.php
+++ app/Services/ObrService.php
@@ -6,16 +6,19 @@ use App\Models\AppConfig;
 use Illuminate\Support\Facades\Http;
 
 class ObrService{
-    public $base_url = "";
+    public $baseUrl = "";
     public function __construct()
     {
      // URL si l'envirenoment est celle de Test
-      $this->base_url = AppConfig::getConfigKey("OBR_MODE_TEST") ?  AppConfig::getConfigKey("OBR_TEST_URL") : AppConfig::getConfigKey("OBR_PROD_URL");
+      $this->baseUrl = AppConfig::getConfigKey("OBR_MODE_TEST") ?  AppConfig::getConfigKey("OBR_TEST_URL") : AppConfig::getConfigKey("OBR_PROD_URL");
+    }
 
+    public function checkTin(){
+        
     }
 
     public function getToken(){
-       $response = Http::post($this->base_url."/login", [
+       $response = Http::post($this->baseUrl."/login", [
         "username"=> AppConfig::getConfigKey("OBR_USERNAME") ,
         "password"=> AppConfig::getConfigKey("OBR_PASSWORD"),
         ]);
@@ -23,7 +26,12 @@ class ObrService{
         if ($response->json()['success']) {
             return $response->json()['result']['token'] ?? null;
         } 
-        return $response;
+        return $response; 
+    }
+
+    private function post($url , $data){
+        $token = $this->getToken();
+        return Http::withToken($token)->post($url, $data);
         
     }
 }
