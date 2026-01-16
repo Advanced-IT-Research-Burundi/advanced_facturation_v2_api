b4d02e11ec33241f6181fae0cbff62459552f46b
Jean-Lionel <nijeanlionel@gmailcom>
2026-01-16 06:13:02 +0200

    stock mouvement controller

--- app/Models/WarehouseTransferItem.php
+++ app/Models/WarehouseTransferItem.php
@@ -2,45 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
-
-class WarehouseTransferItem extends Model
-{
-    protected $fillable = [
-        'transfer_id',
-        'product_id',
-        'quantity',
-        'unit_price',
-        'currency',
-        'stock_movement_out_id',
-        'stock_movement_in_id'
-    ];
-
-    public function transfer()
-    {
-        return $this->belongsTo(WarehouseTransfer::class, 'transfer_id');
-    }
-
-    public function product()
-    {
-        return $this->belongsTo(Product::class);
-    }
-
-    public function movementOut()
-    {
-        return $this->belongsTo(StockMovement::class, 'stock_movement_out_id');
-    }
-
-    public function movementIn()
-    {
-        return $this->belongsTo(StockMovement::class, 'stock_movement_in_id');
-    }
-}
-<?php
-
-namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
